de010fc5e31d56550b625d9a769f2b30017a305d
SumaitaSabaha <sumaitasabaha9@gmail.com>
2023-08-26 14:45:29 -0500

    corrected code so boxes fit in the container

--- sketch.js
+++ sketch.js
@@ -4,11 +4,11 @@ const box = document.querySelector('.container');
 for(let i = 0; i < 16; i++) {
     for(let j = 0; j < 16; j++) {
         const grid = document.createElement('div');
-        grid.style.width = "35px";
-        grid.style.height = "35px";
+        grid.style.width = "30px";
+        grid.style.height = "30px";
         grid.style.border = "1px solid black";
+        grid.style.background = "white"
         box.append(grid);
-
     }
 
 }
