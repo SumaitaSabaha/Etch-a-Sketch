88bc4b446cdcd61fc7e6ec2d0be3852f507cb2c1
SumaitaSabaha <sumaitasabaha9@gmail.com>
2023-08-29 21:21:04 -0500

    updated js to dynamically change size of board

--- sketch.js
+++ sketch.js
@@ -1,19 +1,31 @@
-const box = document.querySelector('.container');
-
-
-for(let i = 0; i < 16; i++) {
-    for(let j = 0; j < 16; j++) {
-        const grid = document.createElement('div');
-        grid.style.width = "30px";
-        grid.style.height = "30px";
-        grid.style.border = "1px solid grey";
-        grid.style.background = "snow"
-        box.append(grid);
-        grid.addEventListener("mouseover", function(){
-            grid.style.background = randomColor();
-        });
+function createBoard(size) {
+    if(size < 16 || size > 100) {
+        alert("Please enter a number greater than 16 and less than 100");
     }
+    else if(size >= 16 || size <= 100) {
+        let box = document.querySelector('.container');
+        let divs = box.querySelectorAll("div")
+        divs.forEach((div) => div.remove());
+        box.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        box.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        for(let i = 0; i < size * size; i++) {
+            const grid = document.createElement('div');
+            grid.style.background = "snow"
+            box.insertAdjacentElement("beforeend", grid);
+            grid.addEventListener("mouseover", function(){
+            grid.style.background = randomColor();
+            });
+        }
+    }
+    
 }
+
+createBoard(16);
+
+function changeSize(input) {
+    createBoard(input);
+}
+
 
 function randomColor() {
     let color = [];
@@ -22,3 +34,4 @@ function randomColor() {
     }
     return 'rgb(' + color.join(', ') + ')';
 }
+
